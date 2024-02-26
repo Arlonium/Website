583f89de22a4adef262974eb511663f6f6b32c7f
AlxFrst <alexandreforestier1107@gmail.com>
2024-02-26 01:17:24 +0100

    Ajouter des fichiers et des dépendances pour les composants Footer et Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>sd</li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
